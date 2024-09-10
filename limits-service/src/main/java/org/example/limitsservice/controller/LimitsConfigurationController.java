e58006d67db641788ecdea1424903a9b8254cb34
HoangHa412 <tunjin123@gmail.com>
2024-09-10 09:16:43 +0700

    first commit

---
+++ limits-service/src/main/java/org/example/limitsservice/controller/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package org.example.limitsservice.controller;
+
+import org.example.limitsservice.Configuration;
+import org.example.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+}
